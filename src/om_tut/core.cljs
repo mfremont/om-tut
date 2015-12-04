9eef42f6de0ba96a5a5f5a360e1211195d705cb1
Matthew Fremont <matthew.fremont@gmail.com>
2015-12-04 07:51:18 -0500

    first component

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -8,16 +8,40 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom
+    {:contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
 
-(om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+     middle (str " " middle)
+     middle-initial (str " " middle-initial ".")))
 
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+            (dom/li nil (display-name contact)))))
+
+(defn contacts-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+            (dom/div nil
+                     (dom/h2 nil "Contact List")
+                     (apply dom/ul nil
+                            (om/build-all contact-view (:contacts data)))))))
+
+(om/root contacts-view app-state
+  {:target (. js/document (getElementById "contacts"))})
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
